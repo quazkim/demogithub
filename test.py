895cbb005422be5b65b917fe202abb432e4c1733
Kim_Quan <kimquanvy2005@gmail.com>
2025-10-21 18:22:46 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import os # Thư viện này được import nhưng không bao giờ dùng
+
+def my_function( a,b ): # Có khoảng trắng thừa trước dấu phẩy
+    c=a+b # Thiếu khoảng trắng quanh dấu = và +
+    return c
+
+print(my_function(1,2))
